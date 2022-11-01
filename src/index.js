3d3441004f45081db7122834ecd57d372fbc5c4a
Amine <ouas.amine@outlook.com>
2022-11-01 13:07:33 +0100

    Style all elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// component file
 import TodoContainer from './components/TodoContainer';
+import './App.css';
 
 ReactDOM.render(<TodoContainer />, document.getElementById('root'));
