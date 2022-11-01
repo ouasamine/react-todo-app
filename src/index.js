382e7ecacbb1803f871142a625c237639a91612f
Amine <ouas.amine@outlook.com>
2022-11-01 12:50:51 +0100

    Create all project components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+// component file
+import TodoContainer from './components/TodoContainer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
